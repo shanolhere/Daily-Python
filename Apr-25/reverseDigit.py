b6893f0e71d6036d5ed5ed6350d806d41f8156de
Sabiya Tabassum <sabiyatabassum007@gmail.com>
2022-04-26 02:26:30 +0530

    apr-26 commit

---
+++ Apr-25/reverseDigit.py
@@ -0,0 +1,4 @@
+'''
+Challenge #1
+Given a number, write a function to output its reverse digits. (e.g. given 123 the answer is 321) Make sure that if it is a negative number you keep the negative in the front (-123 becomes -321)
+'''
